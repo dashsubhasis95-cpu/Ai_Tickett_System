11de91b48e82aa3bf0d3f95dc76bbd7a7455df5d
Subhasis Dash <dashsubhasis95@gmail.com>
2026-04-18 20:39:46 +0530

    Fix onTicketCreated function by adding retries and correcting async syntax

--- ai_assistant_system/inngest/functions/on-ticket-create.js
+++ ai_assistant_system/inngest/functions/on-ticket-create.js
@@ -1,13 +1,17 @@
 export const  onTicketCreated = inngest.createFunction(
-    {id: "on-ticket-created"},
+    {id: "on-ticket-created", retries:2 },
     {event: "ticket/created"},
     async ({event, step}) => {
         try {
 
             // fetch ticket from db
             const {ticketId} = event.data;
-            const ticket = await step.run("fetch-ticket", async () >= {
+            const ticket = await step.run("fetch-ticket", async () => {
+                const ticketObject = await Ticket.findById(ticketId);
+
                 
+
+
             })
         }
 
