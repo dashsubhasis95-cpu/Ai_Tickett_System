821738bb1530f374b3bc9d09973ca44f326fbe84
Subhasis Dash <dashsubhasis95@gmail.com>
2026-04-18 20:34:30 +0530

    Add error handling and ticket fetching logic in onTicketCreated function

--- ai_assistant_system/inngest/functions/on-ticket-create.js
+++ ai_assistant_system/inngest/functions/on-ticket-create.js
@@ -2,7 +2,15 @@ export const  onTicketCreated = inngest.createFunction(
     {id: "on-ticket-created"},
     {event: "ticket/created"},
     async ({event, step}) => {
-        
+        try {
+
+            // fetch ticket from db
+            const {ticketId} = event.data;
+            const ticket = await step.run("fetch-ticket", async () >= {
+                
+            })
+        }
+
 
 }
 )
