bc230d616c48fdf01542772cd5af8d8de356ad3b
Subhasis Dash <dashsubhasis95@gmail.com>
2026-04-18 20:25:52 +0530

    Implement onTicketCreated function for handling ticket creation events

--- ai_assistant_system/inngest/functions/on-ticket-create.js
+++ ai_assistant_system/inngest/functions/on-ticket-create.js
@@ -1,1 +1,7 @@
-export const  onTicketCreated = inngest
+export const  onTicketCreated = inngest.createFunction(
+    {id: "on-ticket-created"},
+    {event: "ticket/created"},
+    async ({event, step}) => {
+
+}
+)
